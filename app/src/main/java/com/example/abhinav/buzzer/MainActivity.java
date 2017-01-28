aad016017c0e4f96ec059e9db73897ba42e8607d
Abhishek <magix21@github.com>
2017-01-28 22:11:40 +0530

    Recycler view order reverted

--- app/src/main/java/com/example/abhinav/buzzer/MainActivity.java
+++ app/src/main/java/com/example/abhinav/buzzer/MainActivity.java
@@ -5,15 +5,11 @@ import android.content.Intent;
 import android.media.Image;
 import android.nfc.Tag;
 import android.support.annotation.NonNull;
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -26,7 +22,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -40,9 +35,6 @@ import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -59,15 +51,11 @@ public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    FragmentTransaction fragmentTransaction;
 
     NavigationView navigationView;
 
-    private LinearLayoutManager mLayoutManager;
-
     Toolbar mtoolbar;
-
-
-    FloatingActionButton mfab;
 
 
     @Override
@@ -88,11 +76,8 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        mfab = (FloatingActionButton) findViewById(R.id.fab);
-
         mtoolbar = (Toolbar) findViewById(R.id.nav_actionBar);
         setSupportActionBar(mtoolbar);
-
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.activity_main) ;
         mToggle= new ActionBarDrawerToggle(this , mDrawerLayout , R.string.open , R.string.close);
@@ -103,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.nav_account:
-                        Intent accountIntent = new Intent(MainActivity.this, ProfileActivity.class);
+                        Intent accountIntent = new Intent(MainActivity.this, YourActivity.class);
                         accountIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                         startActivity(accountIntent);
                         mDrawerLayout.closeDrawers();
@@ -136,8 +121,6 @@ public class MainActivity extends AppCompatActivity {
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
-
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Post");
         mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users");
         mDatabaseLike = FirebaseDatabase.getInstance().getReference().child("Like");
@@ -149,17 +132,6 @@ public class MainActivity extends AppCompatActivity {
         mHomePage = (RecyclerView) findViewById(R.id.Home_Page);
         mHomePage.setHasFixedSize(true);
         mHomePage.setLayoutManager(new LinearLayoutManager(this));
-
-        mfab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent postIntent = new Intent(MainActivity.this, PostActivity.class);
-                postIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(postIntent);
-            }
-        });
-
-
 
 
         checkUserExist();
@@ -242,10 +214,6 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        mLayoutManager = new LinearLayoutManager(MainActivity.this);
-        mLayoutManager.setReverseLayout(true);
-
-        mHomePage.setLayoutManager(mLayoutManager);
         mHomePage.setAdapter(firebaseRecyclerAdapter);
 
 
@@ -360,16 +328,16 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
-       if (item.getItemId() == R.id.action_logout){
+        if (item.getItemId() == R.id.action_add){
+
+            startActivity(new Intent(MainActivity.this , PostActivity.class));
+        }
+
+        if (item.getItemId() == R.id.action_logout){
 
             logout();
         }
 
-        if (item.getItemId() == R.id.action_profile){
-            Intent profileIntent = new Intent(MainActivity.this, ProfileActivity.class);
-            profileIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(profileIntent);
-        }
         return super.onOptionsItemSelected(item);
     }
 
