294a42e9b7335bda06d2e6de6c1aa74d54da1a38
mrw0nd3rfu1 <colabhinov@gmail.com>
2017-02-02 18:32:51 +0530

    changes

--- app/src/main/java/com/example/abhinav/buzzer/LoginActivity.java
+++ app/src/main/java/com/example/abhinav/buzzer/LoginActivity.java
@@ -2,6 +2,8 @@ package com.example.abhinav.buzzer;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
@@ -49,6 +52,10 @@ public class LoginActivity extends AppCompatActivity {
 
     private ProgressDialog mProgress;
 
+    AnimationDrawable animationDrawable;
+    RelativeLayout relativeLayout;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +73,11 @@ public class LoginActivity extends AppCompatActivity {
         mLoginPasswordField = (EditText) findViewById(R.id.loginPasswordfield);
         mLoginBtn = (Button) findViewById(R.id.loginButton);
         mRegisterBtn = (Button) findViewById(R.id.registerButton);
+
+        relativeLayout = (RelativeLayout)findViewById(R.id.activity_login);
+        animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(5000);
+        animationDrawable.setExitFadeDuration(2000);
 
         mProgress = new ProgressDialog(this);
 
@@ -232,5 +244,18 @@ public class LoginActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning())
+            animationDrawable.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning())
+            animationDrawable.stop();
     }
 }
