12ef8435875d0f5c05f5adc97e14adb97559ffdf
Abhishek Jaiswal <magix21@users.noreply.github.com>
2017-02-04 19:29:44 +0530

    pull (#1)
    * changes
    
    * changes
    
    * changes
    
    * changes

--- app/src/main/java/com/example/abhinav/buzzer/RegisterActivity.java
+++ app/src/main/java/com/example/abhinav/buzzer/RegisterActivity.java
@@ -31,8 +31,7 @@ public class RegisterActivity extends AppCompatActivity {
 
     private ProgressDialog mProgress;
 
-    AnimationDrawable animationDrawable;
-    RelativeLayout relativeLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +47,7 @@ public class RegisterActivity extends AppCompatActivity {
         mPasswordField = (EditText) findViewById(R.id.passwordField);
         mRegisterBtn = (Button) findViewById(R.id.registeredButton);
 
-        relativeLayout = (RelativeLayout)findViewById(R.id.activity_login);
-        animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
-        animationDrawable.setEnterFadeDuration(5000);
-        animationDrawable.setExitFadeDuration(2000);
+
 
         mRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,17 +87,5 @@ public class RegisterActivity extends AppCompatActivity {
         }
 
     }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (animationDrawable != null && !animationDrawable.isRunning())
-            animationDrawable.start();
-    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (animationDrawable != null && animationDrawable.isRunning())
-            animationDrawable.stop();
-    }
 }
