48c225f2f747e234f7e8fe1fc9126670ae97b682
Abhishek <magix21@github.com>
2017-02-09 22:29:39 +0530

    commit

--- app/src/main/java/com/example/abhinav/buzzer/SplashScreenActivity.java
+++ app/src/main/java/com/example/abhinav/buzzer/SplashScreenActivity.java
@@ -1,8 +1,8 @@
 package com.example.abhinav.buzzer;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
