19e4abcc37d593145a96f3f76bff40bbf7093e67
mrw0nd3rfu1 <colabhinov@gmail.com>
2017-02-10 20:30:37 +0530

    changes

--- app/src/main/java/com/example/abhinav/buzzer/MainActivity.java
+++ app/src/main/java/com/example/abhinav/buzzer/MainActivity.java
@@ -14,14 +14,17 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.gms.ads.AdListener;
@@ -40,9 +43,9 @@ import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
-    NavigationView navigationView;
     Toolbar mtoolbar;
     FloatingActionButton mfab;
+    private ImageView overflow;
     private RecyclerView mHomePage;
     private DatabaseReference mDatabase;
     private DatabaseReference mDatabaseUsers;
@@ -96,8 +99,6 @@ public class MainActivity extends AppCompatActivity {
         };
 
         mfab = (FloatingActionButton) findViewById(R.id.fab);
-
-
 
         mtoolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mtoolbar);
@@ -198,7 +199,7 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View v) {
                         //Toast.makeText(MainActivity.this , "You clicked a view" , Toast.LENGTH_SHORT).show();
 
-                        Intent singleHomeIntent = new Intent(MainActivity.this, EventActivity.class);
+                        Intent singleHomeIntent = new Intent(MainActivity.this, HomeSingleActivity.class);
                         singleHomeIntent.putExtra("home_id", post_key);
                         startActivity(singleHomeIntent);
                     }
@@ -237,6 +238,8 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 });
+
+
             }
         };
 
@@ -275,6 +278,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
