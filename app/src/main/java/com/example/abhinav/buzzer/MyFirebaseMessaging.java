4f6ffde844fc7c1ef16adb83a0ec02075140ee96
Abhishek Jaiswal <magix21@users.noreply.github.com>
2017-02-16 19:05:03 +0530

    Double card layout (#7)
    * Recycler view order reverted
    
    * Recycler view order reverted
    
    * Recycler view order reverted
    
    * update
    
    * indented
    
    * Recycler view order reverted
    
    * Recycler view order reverted
    
    * update
    
    * indented
    
    * Corrected your activity mistake
    
    * Recycler view order reverted
    
    * update
    
    * indented
    
    * update
    
    * indented
    
    * Corrected your activity mistake
    
    * Corrected your activity mistake
    
    * Changed image delete on post delete
    
    * Changed image delete on post delete
    
    * pull (#1)
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * Reversed Recycler and added PostId's
    
    * Commit
    
    * commit

--- app/src/main/java/com/example/abhinav/buzzer/MyFirebaseMessaging.java
+++ app/src/main/java/com/example/abhinav/buzzer/MyFirebaseMessaging.java
@@ -2,7 +2,6 @@ package com.example.abhinav.buzzer;
 
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
