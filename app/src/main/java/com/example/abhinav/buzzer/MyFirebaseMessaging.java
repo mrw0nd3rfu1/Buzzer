e988c9d1287b11d34820915831658886e73547fc
mrw0nd3rfu1 <colabhinov@gmail.com>
2017-03-15 17:07:39 +0530

    collegeroot added

--- app/src/main/java/com/example/abhinav/buzzer/MyFirebaseMessaging.java
+++ app/src/main/java/com/example/abhinav/buzzer/MyFirebaseMessaging.java
@@ -4,6 +4,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -20,6 +22,7 @@ public class MyFirebaseMessaging extends FirebaseMessagingService {
 
         Intent intent = new Intent(this , MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
 
         PendingIntent pendingIntent = PendingIntent.getActivity(this , 0 ,intent , PendingIntent.FLAG_ONE_SHOT);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
