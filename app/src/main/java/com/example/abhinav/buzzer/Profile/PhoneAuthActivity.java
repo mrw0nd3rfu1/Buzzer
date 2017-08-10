145fe5460cc499aeb24d33ac5e2a224cff9da62e
mrw0nd3rfu1 <colabhinov@gmail.com>
2017-08-11 01:38:02 +0530

    final draft v2

--- app/src/main/java/com/example/abhinav/buzzer/Profile/PhoneAuthActivity.java
+++ app/src/main/java/com/example/abhinav/buzzer/Profile/PhoneAuthActivity.java
@@ -3,6 +3,7 @@ package com.example.abhinav.buzzer.Profile;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
@@ -25,6 +26,7 @@ public class PhoneAuthActivity extends AppCompatActivity {
     private String mVerificationId;
     private String phoneNumber;
     private EditText code;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +38,8 @@ public class PhoneAuthActivity extends AppCompatActivity {
         code.setText("+91");
         code.setEnabled(false);
         mAuth =FirebaseAuth.getInstance();
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        mToolbar.setTitle("Phone Authentication");
 
     }
 
