5da521d43b436f2b6952b0a6b9f4ad2118cc1c05
mrw0nd3rfu1 <colabhinov@gmail.com>
2017-10-10 10:51:25 +0530

    new card layout and other changes

--- app/src/main/java/com/tech/abhinav/buzzer/Clubs/ClubDetails.java
+++ app/src/main/java/com/tech/abhinav/buzzer/Clubs/ClubDetails.java
@@ -1,17 +1,60 @@
 package com.tech.abhinav.buzzer.Clubs;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.tech.abhinav.buzzer.R;
 
 public class ClubDetails extends AppCompatActivity {
 
+    private static final int GALLERY_REQUEST = 1;
+    private Uri imageUri = null;
+    private ImageButton imageSelect;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.club_details);
+
+        imageSelect = (ImageButton) findViewById(R.id.imageButton);
+
+        imageSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_REQUEST);
+            }
+        });
+
+
+
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 1)
+        {
+            this.moveTaskToBack(true);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK) {
+            imageUri=data.getData();
+            imageSelect.setImageURI(imageUri);
+        }
 
     }
 
